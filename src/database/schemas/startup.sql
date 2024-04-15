dc71fa45524d79025e131c338a9480c30dee495c
Habubinator <61784574+Habubinator@users.noreply.github.com>
2024-04-15 21:26:59 +0300

    db startup

---
+++ src/database/schemas/startup.sql
@@ -0,0 +1,47 @@
+CREATE TABLE
+    users (
+        id INT PRIMARY KEY,
+        email VARCHAR(32),
+        username VARCHAR(16),
+        password VARCHAR(16),
+        rating INT,
+        isBanned BOOL,
+        isAdmin BOOL
+    );
+
+CREATE TABLE
+    comments (
+        id INT PRIMARY KEY,
+        author INT,
+        comment_content VARCHAR(512),
+        reply_to_comment INT,
+        FOREIGN KEY (author) REFERENCES users (id),
+        FOREIGN KEY (reply_to_comment) REFERENCES comments (id)
+    );
+
+CREATE TABLE
+    images (id INT PRIMARY KEY, image_href VARCHAR(256));
+
+CREATE TABLE
+    tags (id INT PRIMARY KEY, tag_name VARCHAR(32));
+
+CREATE TABLE
+    news (
+        id INT PRIMARY KEY,
+        title VARCHAR(32),
+        small_desc VARCHAR(256),
+        main_image INT,
+        author INT,
+        news_content VARCHAR(16300),
+        FOREIGN KEY (main_image) REFERENCES images (id),
+        FOREIGN KEY (author) REFERENCES users (id)
+    );
+
+CREATE TABLE
+    news_tags (
+        id INT PRIMARY KEY,
+        tag_id INT,
+        news_id INT,
+        FOREIGN KEY (tag_id) REFERENCES tags (id),
+        FOREIGN KEY (news_id) REFERENCES news (id)
+    );
