8f05ced032c5e8ebba770140ae5208ab3810697a
Habubinator <61784574+Habubinator@users.noreply.github.com>
2024-05-30 21:51:06 +0300

    jwt upd

--- src/index.js
+++ src/index.js
@@ -6,15 +6,22 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
+const checkToken = require("./middlewares/jwt.js");
 const authRouter = require("./routes/authRouter.js");
 // const newsRouter = require("./routes/newsRouter.js");
 // const adminRouter = require("./routes/adminRouter.js");
 app.options("*", cors());
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), "public")));
+app.use(checkToken);
+
 app.use("/auth", authRouter);
 // app.use("/news", newsRouter);
 // app.use("/admin", adminRouter);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(process.cwd(), "public", "html", "main_page.html"));
+});
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
